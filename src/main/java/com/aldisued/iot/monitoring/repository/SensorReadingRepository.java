e3ce7132e8e128af88a5b5d90ccef00a951661fc
Werner Tamás <tamas.werner.work@gmail.com>
2026-08-14 12:37:38 +0200

    Fix typo in averageTemperature method name

--- src/main/java/com/aldisued/iot/monitoring/repository/SensorReadingRepository.java
+++ src/main/java/com/aldisued/iot/monitoring/repository/SensorReadingRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 public interface SensorReadingRepository extends JpaRepository<SensorReading, String> {
     @Query("SELECT avg(sr.value) FROM SensorReading sr WHERE sr.sensor.type = :type AND sr.timestamp BETWEEN :from AND :to")
-    Optional<Double> averageTemerature(LocalDateTime from, LocalDateTime to, SensorType type);
+    Optional<Double> averageTemperature(LocalDateTime from, LocalDateTime to, SensorType type);
 
     @Query("SELECT sr.value FROM SensorReading sr WHERE sr.sensor.type = :type AND sr.timestamp BETWEEN :from AND :to ORDER BY sr.timestamp")
     List<Double> measurementValues(SensorType type, LocalDateTime from, LocalDateTime to);
